90a4234b5c00ac6f02a9a61432e62ada198a0c71
raginigoswami19485 <raginigoswami19485@gmail.com>
2025-02-25 20:14:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// let marks=[98,78,90,76,96];
+// console.log(marks);
+// let data=["ragini,divya,ritik,neelam"];
+// document.write(data);
+
+
+// for(let ok=0; ok < marks.length; 0++) {
+//     console.log(marks[ok]);
+// }
+let marks=[98,78,90,76,96];
+console.log(marks);
+for (let i=0; i<marks.length;++i){
+    console.log(marks[i]);
+}
+let data=["ram,shayam,rohit ,divya "];
+console.log(data);
+for(let imp of data){
+console.log(imp.toUpperCase());
+}
