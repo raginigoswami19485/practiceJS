ef5715a18da6557c6cd64a37287f054a5c2dcdfc
raginigoswami19485 <raginigoswami19485@gmail.com>
2025-02-27 20:14:13 +0530

    enteree

--- index.js
+++ index.js
@@ -7,40 +7,47 @@
 // for(let ok=0; ok < marks.length; 0++) {
 //     console.log(marks[ok]);
 // }
-let marks=[98,78,90,76,96];
-console.log(marks);
+// let marks=[98,78,90,76,96];
+// console.log(marks);
 // for (let i=0; i<marks.length;++i){
 //     console.log(marks[i]);
 // }
-let data=["ram,shayam,rohit ,divya "];
-console.log(data);
-for(let imp of data){
-console.log(imp.toUpperCase());
-}
+// let data=["ram,shayam,rohit ,divya "];
+// console.log(data);
+// for(let imp of data){
+// console.log(imp.toUpperCase());
+// }
 
-for ( let i=1;i<=5;++i ){
-    console.log(" hello i am ragini");
-}
-let str= "ragini";     
-console.log (str.toUpperCase());
-console.log(str.replaceAll("ragini","khushbu"))
-console.log(str)
+// for ( let i=1;i<=5;++i ){
+//     console.log(" hello i am ragini");
+// }
+// let str= "ragini";     
+// console.log (str.toUpperCase());
+// console.log(str.replaceAll("ragini","khushbu"))
+// console.log(str)
+// let fullname = prompt("enter your name");
+// let names= "@" + fullname + fullname.length; 
+// console.log(names);
 
+//     stu1:98,
+//     stu2:99,
 
+// }
+// console.log(student);
+// let names={
+//     plc1:"goa",
+//     plc2:"hopal",
+//     plc3:"hopal",
+//     plc4:"l",
+//     plc5:"opl",
+//     plc6:"pal",
 
+// }
+// console.log(names);
 
+// let marks=[37,45,67,87,87];
+// console.log(marks);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let names= ["am","jjhhg","dfgngini","fdsss","gfhf","fdgfbfhgrgf"]
+names.push("rrrrrr","ggdh");
+console.log(names);
